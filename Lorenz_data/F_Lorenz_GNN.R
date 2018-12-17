a4d1082fc750445f243638412f78fb54c93a8e3d
AlbertChen <albirtle93@gmail.com>
2018-12-17 15:29:51 +0900

    Finish LORENZ GNN function

--- Lorenz_data/F_Lorenz_GNN.R
+++ Lorenz_data/F_Lorenz_GNN.R
@@ -10,7 +10,7 @@ DimData<-function(data,n){
         GNN[,1]<-data[,1]
         for( i in c(1:(nrow(data)))){
                 for(j in c(1:n)){
-                        GNN[i,j]<-data[(i+j),1]    
+                        GNN[i,j]<-data[(i+j-1),1]    
                 }
             }
         return(GNN)
@@ -21,23 +21,23 @@ tail(DimData(data = LORENZ,n = 4))
 ##second calculate dist
 Dist<-function(data,n){
         mind<-matrix(nrow = 1,ncol = 3,100)
-        dist<-0
-        GNN<-DimData(data = LORENZ,n = n)
-        GNN<-GNN[1:(nrow(GNN)-n),]
-        for (i in 1:(length(GNN[,1])-2)){
-                dm<-matrix(nrow =(length(GNN[,1])-(i+1)),ncol = n)# problem from here
+        GN<-DimData(data = data,n = n)
+        
+        for (i in 1:(length(GN[,1])-(n+2))){
+                GNN<-GN[1:(nrow(GN)-n),]
+                
                 for (j in 1:n){
                         
-                        dm[,j]<-GNN[(i+1):(length(GNN[,1])),j]-GNN[i,j]
+                        GNN[(i+1):(length(GNN[,1])),j]<-GNN[(i+1):(length(GNN[,1])),j]-GNN[i,j]
                         
                                 }
                 dist<-0
                 for(k in 1:n){
-                        dist<-dist+na.omit(dm[,k])^2
+                        dist<-dist+na.omit(GNN[,k])^2 #problem dist+ give empty back
                 }
                 if (mind[,3] > min(dist)){
                                 mind[,1]<-i
-                                mind[,2]<-which.min(dist)+i
+                                mind[,2]<-which.min(dist)
                                 mind[,3]<-min(dist)
                                             }
                 
@@ -45,23 +45,25 @@ Dist<-function(data,n){
         
         return(mind)
 }
-
+result<-Dist(data = LORENZ,n = 2)
 #solution 1 , reload data(put GNN in the first for loop)
-
-
-mind
+result
 #third calculate one more dimension
-G3<-matrix(nrow = 16384,ncol=3)
-G3[,1]<-GNN[,1]
-G3[,2]<-GNN[,2]
+#4080 10035 
+G4<-DimData(LORENZ,3)
+fd<-G3[4082,]-G3[5957,]
+tind<-sum(fd^2)
+sqrt((tind-result[,3])/result[,3])
+abs(LORENZ[4084,]-LORENZ[5959,])^2/result[,3]
+tind#
+#PLOT   from 2 to 10 
 
-for( i in c(1:16384)){
+rs<-list()
+for (i in 1:9){
         
-        G3[i,3]<-LORENZ[i+2,1]
+        d<-Dist(data = LORENZ,n=i+1)
+        rs[i]<-abs(LORENZ[d[1]+i+1,]-LORENZ[d[2]+i+1,])^2/d[,3]
+        print(rs[i])
         
 }
-G3<-G3[1:16382,]
-td<-G3[mind[,1],]-G3[mind[,2],]
-tind<-td[1]^2+td[2]^2+td[3]^2
-tind/mind[,3]
-#
+plot(rs,type = "l")
